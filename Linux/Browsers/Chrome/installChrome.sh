5cd5742198fd18638fc77f3fa4b65f778763d7df
Vivien Ogoun <vivienogoun1@gmail.com>
2023-08-01 12:15:01 +0100

    [fix] Chrome installation on Linux

--- Linux/Browsers/Chrome/installChrome.sh
+++ Linux/Browsers/Chrome/installChrome.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-apt-get update && apt-get upgrade
+echo "------------------------------------------------------------------------------"
+echo "apt-get update"
+echo "------------------------------------------------------------------------------"
+apt-get update
 
-apt-get install -f
+echo "------------------------------------------------------------------------------"
+echo "apt-get upgrade"
+echo "------------------------------------------------------------------------------"
+apt-get upgrade
 
+echo "------------------------------------------------------------------------------"
+echo "wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+echo "------------------------------------------------------------------------------"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
+echo "------------------------------------------------------------------------------"
+echo "dpkg -i --force-depends google-chrome-stable_current_amd64.deb"
+echo "------------------------------------------------------------------------------"
 dpkg -i --force-depends google-chrome-stable_current_amd64.deb
 
+echo "------------------------------------------------------------------------------"
+echo "rm google-chrome-stable_current_amd64.deb"
+echo "------------------------------------------------------------------------------"
 rm google-chrome-stable_current_amd64.deb
 
-echo "Chrome Successful Installed "
+echo "Chrome Successfully Installed "
 
+echo "------------------------------------------------------------------------------"
+echo "google-chrome"
+echo "------------------------------------------------------------------------------"
 google-chrome
