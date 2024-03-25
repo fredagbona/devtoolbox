d9652344ebd5ff8a4f3c283688865291e0333972
fredagbona <fredagbona@gmail.com>
2024-03-25 21:00:54 +0100

    [UPDATE] : composer script

--- Linux/Programming/Composer/installComposer.sh
+++ Linux/Programming/Composer/installComposer.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 sudo apt update
-sudo apt-get install curl unzip
-sudo apt-get install php php-curl php-zip php-xml
-sudo apt-get install php-cli unzip
-curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
-sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
-composer
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+rm composer-setup.php
 echo "Composer Successful Installed "
 exit 0
